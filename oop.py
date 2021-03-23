591857bb144d5ffa2f2cb3f3ca4cefd84f5de9e6
rizky maulana <ikywho22@gmail.com>
2021-03-23 16:45:39 +0700

    Update .gitignore

--- oop.py
+++ oop.py
@@ -1,13 +1,4 @@
-#hitung luas persegi
-p = 9
-l = 7
+from controller import MahasiswaController
 
-hasil = p * l
-print(hasil)
-
-def hitung_persegi():
-    p = int(input('input p :'))
-    l = int(input('input l :'))
-    hasil = p*l
-    return hasil
-print(hitung_persegi())
+a = MahasiswaController()
+a.run()
