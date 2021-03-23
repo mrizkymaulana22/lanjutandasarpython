75f568a9ed9307464314dfb6228ae66c096ab050
M. Rizky Maulana <74039226+mrizkymaulana22@users.noreply.github.com>
2021-03-23 13:32:06 +0700

    Update .gitignore

---
+++ oop.py
@@ -0,0 +1,13 @@
+#hitung luas persegi
+p = 9
+l = 7
+
+hasil = p * l
+print(hasil)
+
+def hitung_persegi():
+    p = int(input('input p :'))
+    l = int(input('input l :'))
+    hasil = p*l
+    return hasil
+print(hitung_persegi())
